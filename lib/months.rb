cd7d75e518783d8a6be47996498261ef79b01fa7
Andrew Szczepanski <aszczepanski@financeit.io>
2021-02-02 14:11:53 -0500

    Skeleton for stubs and mocks

--- lib/months.rb
+++ lib/months.rb
@@ -1,21 +1,19 @@
 class Months
-  DAYS_IN_MONTH = {
-    "January" => 31,
-    "February" => Time.days_in_month(2, Time.now.year),
-    "March" => 31,
-    "April" => 30,
-    "May" => 31,
-    "June" => 30,
-    "July" => 31,
-    "August" => 31,
-    "September" => 30,
-    "October" => 31,
-    "November" => 30,
-    "December" => 31,
-  }
-
   def self.average(month_1, month_2, month_3)
-
+    days_in_month = {
+      "January" => 31,
+      "February" => Time.days_in_month(2, Time.now.year),
+      "March" => 31,
+      "April" => 30,
+      "May" => 31,
+      "June" => 30,
+      "July" => 31,
+      "August" => 31,
+      "September" => 30,
+      "October" => 31,
+      "November" => 30,
+      "December" => 31,
+    }
   end
 
   def self.correct_name?(month)
--- spec/lib/months_spec.rb
+++ spec/lib/months_spec.rb
@@ -3,17 +3,6 @@ require_relative '../../lib/months'
 
 RSpec.describe Months do
   describe ".average" do
-    # happy path
-    it "" do
-      # Months.average(args...)
-    end
-
-    # invariant
-    # eg. passing a string that isnt a month
-    it "" do
-
-    end
-
     it "works with a leap year" do
 
     end
